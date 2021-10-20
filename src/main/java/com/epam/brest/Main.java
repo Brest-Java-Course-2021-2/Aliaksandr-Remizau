d9ee3f8e935bb4066495110dbd0fe0eeace01c6f
Alex Remizov <rem.remizov@yandex.ru>
2021-10-20 14:52:51 +0300

    Add third iteration calculator

--- src/main/java/com/epam/brest/Main.java
+++ src/main/java/com/epam/brest/Main.java
@@ -5,13 +5,18 @@ import com.epam.brest.model.Status;
 import com.epam.brest.model.StatusType;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] arg) throws IOException {
 
+        /*String f1 = "weight_price.csv";
+        if(arg.length >0 && arg[0].length()>0){
+            f1 = arg[0];
+        }*/
         Map<Integer,BigDecimal> pricePerKmMap = new CSVFileReader().readFileFromResources("weight_price.csv");
         Map<Integer,BigDecimal> pricePerKgMap = new CSVFileReader().readFileFromResources("distance_price.csv");
 
